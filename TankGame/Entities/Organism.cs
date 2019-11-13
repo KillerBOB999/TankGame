befd986c76b29ef0a96543462933234a3d1683a8
Anthony Harris <33495441+KillerBOB999@users.noreply.github.com>
2019-11-13 11:59:56 -0600

    Added organism class

---
+++ TankGame/Entities/Organism.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TankGame.Entities
+{
+    public class Organism
+    {
+        public NeuralNetwork botBrain;
+        public int brainID;
+
+        public Organism(NeuralNetwork NN, int ID)
+        {
+            botBrain = NN;
+            brainID = ID;
+        }
+    }
+}
